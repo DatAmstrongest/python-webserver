2cd6225d5e18453665682f4a02ef5572cd371c96
DatAmstrongest <metehan1.proj201@gmail.com>
2022-07-29 19:51:03 +0300

    index.html is updated

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM python:3.6
 
 ADD app.py .
 
+ADD index.html .
+
 RUN pip install prometheus_client
 
 CMD ["python", "./app.py"]
--- app.py
+++ app.py
@@ -5,13 +5,13 @@ HIT_COUNT = Counter("hit_count", "total app http request count", ["app_name", "e
 metrics_app = make_wsgi_app()
 
 def my_app(environ, start_fn):
-    print(start_fn)
-    print(environ)
     if environ['PATH_INFO'] == '/metrics':
         return metrics_app(environ, start_fn)
     HIT_COUNT.labels("prom_python_app",environ['PATH_INFO']).inc()
     start_fn('200 OK', [])
-    return [b'Hello World']
+    with open('index.html') as f:
+      content = f.read()
+    return [bytes(content, "utf-8")]
 
 if __name__ == '__main__':
     httpd = make_server('0.0.0.0', 8080, my_app)
